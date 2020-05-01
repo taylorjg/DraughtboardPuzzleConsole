5e9227c60ec70e31227cd2bca222ead8fe784c2d
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-05-01 01:16:10 +0100

    Implement nicer output format using box chars

--- Program.cs
+++ Program.cs
@@ -6,39 +6,11 @@ namespace DraughtboardPuzzleConsole
 {
   class Program
   {
-    private static void DrawSolution(List<PiecePlacement> solution)
-    {
-      char FindPieceNameAt(int x, int y)
-      {
-        foreach (var piecePlacement in solution)
-        {
-          var xWithinPiece = x - piecePlacement.Coords.X;
-          var yWithinPiece = y - piecePlacement.Coords.Y;
-          if (xWithinPiece >= 0 && xWithinPiece < piecePlacement.RotatedPiece.Width &&
-              yWithinPiece >= 0 && yWithinPiece < piecePlacement.RotatedPiece.Height)
-          {
-            var square = piecePlacement.RotatedPiece.SquareAt(xWithinPiece, yWithinPiece);
-            if (square != null)
-            {
-              return piecePlacement.RotatedPiece.Piece.Name;
-            }
-          }
-        }
-        return '?';
-      }
-
-      foreach (var y in Enumerable.Range(0, 8))
-      {
-        var names = Enumerable.Range(0, 8).Select(x => FindPieceNameAt(x, y));
-        Console.WriteLine(string.Concat(names));
-      }
-    }
-
     static void Main(string[] args)
     {
       var solver = new Solver();
       var solution = solver.Solve().ToList();
-      DrawSolution(solution);
+      Draw.DrawSolution(solution);
     }
   }
 }
